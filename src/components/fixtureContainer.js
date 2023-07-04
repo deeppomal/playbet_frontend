34def7121db3221089f58f07e45bb96d9f3698ac
deeppomal <deeppomal98@gmail.com>
2023-07-04 15:54:47 -0400

    filtering league feature added

--- src/components/fixtureContainer.js
+++ src/components/fixtureContainer.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { useFootballAPI } from '../hooks/useFootballAPI'
 import { FixtureCard } from './FixtureCard'
 import { useSelector, } from 'react-redux';
@@ -10,15 +10,23 @@ import { LeagueCard } from './leagueCard';
 export const FixtureContainer = ({toggleBettingModal}) => {
 
   const dateStored = useSelector(getDate);
+  const [selectedLeagues,setSelectedLeagues] = useState({})
+  const [fixturelist,setFixtureList] = useState([])
+  const [allLeagues,setAllLeagues] = useState([])
   const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures';
   const {data,isError,error,isLoading} = useFootballAPI('fixtures',url,dateStored,'3')
   // const {data,isError,error,isLoading} = useFootballAPI('fixtures',url,dateStored,'960')
 
+  useEffect(()=>{
+    setFixtureList(getFilteredFixtures())
+    setAllLeagues(getFilteredFixtures())
+  },[data])
+
   const getFilteredFixtures = () =>{
     let fixtures = data?.data?.response
-    fixtures = fixtures.filter( fixture => (new Date(fixture?.fixture?.timestamp * 1000)) > (new Date()))
+    fixtures = fixtures?.filter( fixture => (new Date(fixture?.fixture?.timestamp * 1000)) > (new Date()))
     let groupedFixtures = {}
-    for(let i = 0; i<fixtures.length; i++){
+    for(let i = 0; i<fixtures?.length; i++){
       groupedFixtures[fixtures[i].league.id] = groupedFixtures[fixtures[i].league.id] ? [...groupedFixtures[fixtures[i].league.id],fixtures[i]] : [fixtures[i]]
     }
     const leagueCodes = ['1','3','39','140','135','78','61'];
@@ -33,7 +41,6 @@ export const FixtureContainer = ({toggleBettingModal}) => {
     }
     return filteredFixtures
   }
-
   if(isLoading){
     return(
       <div className='mt-10'>
@@ -41,22 +48,52 @@ export const FixtureContainer = ({toggleBettingModal}) => {
       </div>
     )
   }
-  if(getFilteredFixtures().length < 1) {
+  if(fixturelist.length < 1) {
     return(
       <div className='mt-10'>
         <p className='text-center font-semibold text-xl text-[#dbd9d8]'>There are no fixtures today</p>
       </div>
     )
   }
-  console.log('fixx', getFilteredFixtures())
+  const handleSelectedLeague = (id) => {
+    let leagueIds = selectedLeagues
+    leagueIds[id] = leagueIds[id] ? leagueIds[id] + 1 : 1
+    setSelectedLeagues(leagueIds)
+    filterLeagueFixtures()
+  }
+  const filterLeagueFixtures = () => {
+    let leagues = []
+    let filteredFixtures = []
+    let allFixtures = getFilteredFixtures()
+    for(let i in selectedLeagues){
+      if(selectedLeagues[i] % 2 !=0){
+        leagues.push(i)
+      }
+    }
+    
+    for(let i in leagues){
+      for(let j in allFixtures){
+        if(leagues[i] == allFixtures[j][0].id){
+          filteredFixtures.push(allFixtures[j])
+          break
+        }
+      }
+    }
+    if(leagues.length == 0){
+      setFixtureList(allFixtures)
+    }
+    else{
+      setFixtureList(filteredFixtures)
+    }
+  }
   return (
     <div className='w-full mt-5 rounded-md flex p-1  '>
       <div className='flex w-1/4 px-4'>
         <div className='w-full bg-[#0a1f3c] rounded-lg mt-3'>
           {
-            getFilteredFixtures().map((data) => {
+            allLeagues.map((data) => {
               return(
-                <LeagueCard data={data}/>
+                <LeagueCard data={data} handleSelectedLeague = {handleSelectedLeague} />
               )
             })
           }
@@ -64,7 +101,7 @@ export const FixtureContainer = ({toggleBettingModal}) => {
       </div>
       <div className='flex w-2/4 items-center flex-col'>
         {
-          getFilteredFixtures().map((data, index) => {
+          fixturelist.map((data, index) => {
             return(
               <FixtureLeagueCard toggleBettingModal={toggleBettingModal} data={data} />
             )
--- src/components/leagueCard.js
+++ src/components/leagueCard.js
@@ -1,10 +1,14 @@
 import React,{useState} from 'react'
 
-export const LeagueCard = ({data}) => {
+export const LeagueCard = ({data,handleSelectedLeague}) => {
     const [isSelected,setIsSelected] = useState(false)
+    const handleOnClick = () => {
+      setIsSelected(!isSelected)
+      handleSelectedLeague(data[1][0].league.id)
+    }
   return (
     <div className={`flex items-center  my-3 p-5 mx-3 rounded-lg cursor-pointer ${isSelected ? 'bg-[#071429]' : 'bg-[#0a1f3c]'}`}
-    onClick={()=>setIsSelected(!isSelected)}>
+    onClick={handleOnClick}>
         <div className=''>
             <img src={data[1][0].league.logo} className='w-11 h-11' />
         </div>
